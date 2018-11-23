3f5838f4073f180136480bd0b1696a67352962be
이향미 <@LAPTOP-7SJ9A9V6>
2018-11-23 11:49:54 +0900

    첫 업로드입니다. 最初のアプロードです。

---
+++ MyGit/src/com/lec/java/GitClass.java
@@ -0,0 +1,15 @@
+package com.lec.java;
+/**
+ *  이 클래스는 Git 업로드 테스트 용입니다.
+ * @author 이향미
+ * 
+ * 
+ */
+public class GitClass {
+
+	public static void main(String[] args) {
+		System.out.println("GitHub에 올리기");
+
+	}
+
+}
